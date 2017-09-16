5bfa75fd109bda2d8a4fe3aeb420e6bae3c24cf1
wayne <wayne.5540@gmail.com>
2017-09-16 15:53:47 +0800

    ip detector

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+'use strict';
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+const {app, BrowserWindow, Menu, Tray} = require('electron')
+require('isomorphic-fetch');
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+let tray = null
 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+const creatTray = () => {
+  tray = new Tray('./green-earth.png')
+  tray.setToolTip('This is my application.')
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
+  fetch("http://ip.jsontest.com/").then((response) => {
+    return response.json()
+  }).then((json) => {
+    tray.setTitle(json.ip)
+  }).catch((error) => {
+    console.log("ERROR:", error)
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+
+app.on('ready', creatTray)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -46,10 +33,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
+  if (tray === null) {
+    creatTray()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
