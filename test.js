b20c218c5b1be333b3a39aeb04e251ad2ca7f92c
wayne <wayne.5540@gmail.com>
2017-09-18 11:04:54 +0800

    temp

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 const { getTrending } = require('./bitfinexApi')
+const { Notification } = require('electron')
 
 getTrending().then((res) => console.log(res))
+
+
+console.log(new Notification({ title: "jijo" }))
